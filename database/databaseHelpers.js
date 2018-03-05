72d9694425095c56ee56eef23e8055a2f3fe5bb6
Jerry Chen <jieningjchen@gmail.com>
2018-03-05 17:26:15 -0500

    Changing login page to include signup button if not signed up and updating schema for claimers

--- database/databaseHelpers.js
+++ database/databaseHelpers.js
@@ -2,11 +2,9 @@ var mysql = require('mysql');
 var config = require('../config.js')
 
 var connection = mysql.createConnection({
-  host: config.host,
-  user: config.user,
-  password: config.password,
-  port: config.port,
-  database: config.database
+  host: 'localhost',
+  user: 'root',
+  database: 'marlowe'
 });
 
 connection.connect((err) => {
@@ -22,41 +20,41 @@ connection.connect((err) => {
 // if (err) console.log(err)
 // })
 
-connection.query(`CREATE TABLE IF NOT EXISTS provider (
-id INTEGER AUTO_INCREMENT NOT NULL,providerUsername VARCHAR(16),
-pPassword VARCHAR(16),PRIMARY KEY(id));`
-, (err, results) => {
-  if (err) console.log(err)
-})
-
-connection.query(`CREATE TABLE IF NOT EXISTS claimer (
-  id INTEGER AUTO_INCREMENT NOT NULL,
-  claimerUsername VARCHAR(16),
-  claimerZipCode INTEGER NOT NULL,
-  cPassword VARCHAR(16),
-  PRIMARY KEY (id)
-);`, (err, res) => {
-if (err) console.log(err)
-})
-
-connection.query(`CREATE TABLE IF NOT EXISTS post (
-  id INTEGER AUTO_INCREMENT NOT NULL,
-  title VARCHAR(100),
-  username VARCHAR(20),
-  description VARCHAR(255),
-  address VARCHAR(50),
-  city VARCHAR(25),
-  state VARCHAR(2),
-  zipCode VARCHAR(6),
-  phone VARCHAR(12),
-  isClaimed BOOLEAN,
-  emailAddress VARCHAR(50),
-  createdAt INTEGER,
-  photoUrl VARCHAR(512),
-  PRIMARY KEY (id)
-)`, (err, res) => {
-if (err) console.log(err)
-})
+// connection.query(`CREATE TABLE IF NOT EXISTS provider (
+// id INTEGER AUTO_INCREMENT NOT NULL,providerUsername VARCHAR(16),
+// pPassword VARCHAR(16),PRIMARY KEY(id));`
+// , (err, results) => {
+//   if (err) console.log(err)
+// })
+//
+// connection.query(`CREATE TABLE IF NOT EXISTS claimer (
+//   id INTEGER AUTO_INCREMENT NOT NULL,
+//   claimerUsername VARCHAR(16),
+//   claimerZipCode INTEGER NOT NULL,
+//   cPassword VARCHAR(16),
+//   PRIMARY KEY (id)
+// );`, (err, res) => {
+// if (err) console.log(err)
+// })
+//
+// connection.query(`CREATE TABLE IF NOT EXISTS post (
+//   id INTEGER AUTO_INCREMENT NOT NULL,
+//   title VARCHAR(100),
+//   username VARCHAR(20),
+//   description VARCHAR(255),
+//   address VARCHAR(50),
+//   city VARCHAR(25),
+//   state VARCHAR(2),
+//   zipCode VARCHAR(6),
+//   phone VARCHAR(12),
+//   isClaimed BOOLEAN,
+//   emailAddress VARCHAR(50),
+//   createdAt INTEGER,
+//   photoUrl VARCHAR(512),
+//   PRIMARY KEY (id)
+// )`, (err, res) => {
+// if (err) console.log(err)
+// })
 
 
 module.exports = connection;
