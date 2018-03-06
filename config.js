296ec0af2e0ba2b25f4b9f1d1c701517b3c72bcb
Riley Alsman <rileyalsman@gmail.com>
2018-03-05 19:44:58 -0500

    Commit before rebase

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleAPI: 'AIzaSyB4xGNkuXIf0eeYQPJ0UuF-eGg_s-TJIQw',
+  charityNav: '57eccc97f21c120357f26376eaf348f2',
   host: 'greenfield-marlowe.coxryxwvinqh.us-east-1.rds.amazonaws.com',
   user: 'marlowe',
   password: 'plantlife',
