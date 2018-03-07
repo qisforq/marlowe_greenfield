40ffe188b7cbcdf83a61dbe820941045fa6c09c3
Quentin Vidal <qisforq@gmail.com>
2018-03-07 11:01:39 -0500

    navbar bugfixes

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -12,21 +12,25 @@ class NavigationBar extends React.Component {
   }
 
   render () {
+    const { scrollTo, onLogout } = this.props;
+
     return (
       <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#home">Kindly</a>
-
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          <NavItem eventKey={1} href="#" onClick={() => this.props.scrollTo()}>
+          <NavItem eventKey={1} href="#" onClick={scrollTo}>
             Recent Posts
           </NavItem>
         </Nav>
         <Nav pullRight>
-          <NavItem eventKey={1} href="#" onClick={() => this.props.onLogout()}>
+          <NavItem eventKey={3} href="#" onClick={scrollTo}>
+            Settings
+          </NavItem>
+          <NavItem eventKey={2} href="#" onClick={onLogout}>
           Log Out
           </NavItem>
         </Nav>
