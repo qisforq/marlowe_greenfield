3d77ed852b1ad7d56da8ce31dce1a7f1eacd2c82
Jerry Chen <jieningjchen@gmail.com>
2018-03-09 13:47:31 -0500

    Editing Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,29 @@ var DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
   entry: SRC_DIR + '/index.jsx',
   output: {
-    filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
+    filename: 'bundle.js'
   },
   module : {
-    loaders : [
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',
-        options: {
-          presets: ['react', 'es2015'],
-          cacheDirectory: false
-       }
+        exclude: /node_modules/,
+        test: /\.jsx?/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['react', 'es2015']
+            }
+          }
+        ]
+      },
+      {
+        resolve: {
+          alias: {
+            'react': path.resolve(__dirname, './node_modules', 'react')
+          }
+        }
       }
     ]
   }
